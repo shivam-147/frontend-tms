8d613c3e819f5bf3da3894b6551a860bcd124431
shivam <shiva147m@gmail.com>
2025-07-29 19:04:27 +0530

    loader 2.0

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 
 function Loader() {
     return (
-        <div className="flex justify-center items-center">
-            <div class="relative">
-                <div className="w-12 h-12 rounded-full absolute border-8 border-dashed border-gray-200"></div>
-
-                <div className="w-12 h-12 rounded-full animate-spin absolute border-8 border-dashed border-purple-500 border-t-transparent">
-                </div>
+        <div className="fixed inset-0 flex justify-center items-center z-50 bg-white bg-opacity-50">
+            <div className="relative">
+                <div className="w-20 h-20 rounded-full absolute border-8 border border-gray-300"></div>
+                <div className="w-20 h-20 rounded-full animate-spin absolute border-8 border border-purple-700 border-t-transparent"></div>
             </div>
-
         </div>
     )
 }
